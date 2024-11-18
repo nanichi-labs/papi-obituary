a37a1e81c8d956c865443f55875230c7df12f392
Ang Centeno <me@angelcenteno.com>
2024-11-18 18:29:00 -0500

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -81,6 +81,10 @@ export default function App() {
 							</div>
 							<h1 className={`text-7xl md:text-9xl ${corinthia.className} text-blue-900 mb-6`}>Angel Centeno</h1>
 							<p className={`font-bold uppercase tracking-widest relative z-10 mb-10 bg-white py-2`}>Feb. 27, 1966 - Nov. 14, 2024</p>
+
+							<a href="#donate" className="mb-4 bg-blue-900 text-white rounded-md py-3 px-10 mr-2 uppercase text-1xl inline-block hover:bg-black transition-colors">Donate</a>
+							<a href="#location" className="mb-4 bg-blue-800 text-white rounded-md py-3 px-10 uppercase text-1xl inline-block hover:bg-black transition-colors">Location</a>
+
 						</div>
 						<div className={`col-span-4 text-[1.2em] md:text-[1.4em]`}>
 							<div className={`max-w-[800px] m-auto mb-20`}>
@@ -94,55 +98,55 @@ export default function App() {
 					</div>
 				</div>
 
-				<div className={`bg-white`}>
-					<div className={`container max-w-[1200px] m-auto py-20`}>
-						<div className={`grid grid-cols-6 gap-8 text-[1.1em]`}>
-							<div className={`col-span-6 mb-10`}>
-								<div className={`w-100 max-w-[600px] m-auto`}>
-									<div className="mb-2">
-										<h2 className={`text-lg font-bold mb-0 uppercase`}>Goal</h2>
-										<p>{percent}% raised of <span className="text-4xl">${Number(goal).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</span></p>
-									</div>
-									<div className="w-100 bg-gray-200 h-4 relative rounded-full overflow-hidden border-solid border-2 border-blue-900">
-										<div className={`absolute z-10 top-0 left-0 h-full bg-blue-900`} style={{ width: `${percent}%` }}></div>
+				<div id="location" className={`bg-white`}>
+					<div className={`container m-auto`}>
+						<div className={`grid grid-cols-4 gap-4`}>
+							<div className={`col-span-4`}>
+								<div className={`max-w-[800px] m-auto mb-20 py-20 text-3xl`}>
+									<p className={`text-lg uppercase font-bold`}>Date</p>
+									<p className={`mb-10`}>Saturday, November 23rd, 2024</p>
+									<p className={`text-lg uppercase font-bold`}>Time</p>
+									<p className={`mb-10`}>4 PM - 8 PM</p>
+									<p className={`text-lg uppercase font-bold`}>Location</p>
+									<p>Funeraria San Juan / San Juan Funeral Home</p>
+									<p className={`mb-10`}>3189 S John Young Pkwy,<br />Kissimmee, FL 34746</p>
+									
+									<p className={`text-lg uppercase font-bold`}>Map</p>
+									<div className="w-full pt-[56.25%] relative">
+										<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d112421.06745434884!2d-81.46735352011142!3d28.29352671968435!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88dd837f4bda75f9%3A0x1ea308684a5064ec!2sFuneraria%20San%20Juan%20Funeral%20Home%20-%20Young%20Parkway!5e0!3m2!1sen!2sus!4v1731966650567!5m2!1sen!2sus" className="w-full h-full absolute inset-0" />
 									</div>
 								</div>
-							</div>
-							<div className={`col-span-6 md:col-span-2`}>
-								<h2 className={`text-lg font-bold mb-0 uppercase`}>Donate with Zelle</h2>
-								<p>Please send Zelle donations to <strong>407-338-9299</strong> or use this QR code:</p>
-								<img className="w-[200px] m-auto" src="https://papiobituary.s3.us-east-1.amazonaws.com/zelle-mami.jpg" />
-							</div>
-							<div className={`col-span-6 md:col-span-2`}>
-								<h2 className={`text-lg font-bold mb-0 uppercase`}>Donate with PayPal</h2>
-								<p>Please send PayPal donations to <strong>jeisleyann@gmail.com</strong></p>
-							</div>
-							<div className={`col-span-6 md:col-span-2`}>
-								<h2 className={`text-lg font-bold mb-0 uppercase`}>Donate with Cash App</h2>
-								<p>Please send CashApp donations to <strong>$angcenteno</strong></p>
-								<img className="w-[250px] m-auto" src="https://papiobituary.s3.us-east-1.amazonaws.com/cashapp.jpg" />
 							</div>
 						</div>
 					</div>
 				</div>
 
-				<div className={`container m-auto`}>
-					<div className={`grid grid-cols-4 gap-4`}>
-						<div className={`col-span-4`}>
-							<div className={`max-w-[800px] m-auto mb-20 py-20 text-3xl`}>
-								<p className={`text-lg uppercase font-bold`}>Date</p>
-								<p className={`mb-10`}>Saturday, November 23rd, 2024</p>
-								<p className={`text-lg uppercase font-bold`}>Time</p>
-								<p className={`mb-10`}>4 PM - 8 PM</p>
-								<p className={`text-lg uppercase font-bold`}>Location</p>
-								<p>Funeraria San Juan / San Juan Funeral Home</p>
-								<p className={`mb-10`}>3189 S John Young Pkwy,<br />Kissimmee, FL 34746</p>
-								
-								<p className={`text-lg uppercase font-bold`}>Map</p>
-								<div className="w-full pt-[56.25%] relative">
-									<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d112421.06745434884!2d-81.46735352011142!3d28.29352671968435!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88dd837f4bda75f9%3A0x1ea308684a5064ec!2sFuneraria%20San%20Juan%20Funeral%20Home%20-%20Young%20Parkway!5e0!3m2!1sen!2sus!4v1731966650567!5m2!1sen!2sus" className="w-full h-full absolute inset-0" />
+				<div id="donate" className={`container max-w-[1200px] m-auto py-20`}>
+					<div className={`grid grid-cols-6 gap-8 text-[1.1em]`}>
+						<div className={`col-span-6 mb-10`}>
+							<div className={`w-100 max-w-[600px] m-auto`}>
+								<div className="mb-2">
+									<h2 className={`text-lg font-bold mb-0 uppercase`}>Goal</h2>
+									<p>{percent}% raised of <span className="text-4xl">${Number(goal).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</span></p>
+								</div>
+								<div className="w-100 bg-gray-200 h-4 relative rounded-full overflow-hidden border-solid border-2 border-blue-900">
+									<div className={`absolute z-10 top-0 left-0 h-full bg-blue-900`} style={{ width: `${percent}%` }}></div>
 								</div>
 							</div>
+						</div>
+						<div className={`col-span-6 md:col-span-2`}>
+							<h2 className={`text-lg font-bold mb-0 uppercase`}>Donate with Zelle</h2>
+							<p>Please send Zelle donations to <strong>407-338-9299</strong> or use this QR code:</p>
+							<img className="w-[200px] m-auto" src="https://papiobituary.s3.us-east-1.amazonaws.com/zelle-mami.jpg" />
+						</div>
+						<div className={`col-span-6 md:col-span-2`}>
+							<h2 className={`text-lg font-bold mb-0 uppercase`}>Donate with PayPal</h2>
+							<p>Please send PayPal donations to <strong>jeisleyann@gmail.com</strong></p>
+						</div>
+						<div className={`col-span-6 md:col-span-2`}>
+							<h2 className={`text-lg font-bold mb-0 uppercase`}>Donate with Cash App</h2>
+							<p>Please send CashApp donations to <strong>$angcenteno</strong></p>
+							<img className="w-[250px] m-auto" src="https://papiobituary.s3.us-east-1.amazonaws.com/cashapp.jpg" />
 						</div>
 					</div>
 				</div>
